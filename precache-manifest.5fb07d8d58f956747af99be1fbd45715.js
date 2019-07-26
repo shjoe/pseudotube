6f0eaff609298708dff90a1f09f1165addda2d3a
shjoe <shure.joe33@gmail.com>
2019-07-25 18:52:26 -0700

    Updates

---
+++ precache-manifest.5fb07d8d58f956747af99be1fbd45715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927a7ae9ea4280ee80920d8985aab0c8",
+    "url": "/pseudotube/index.html"
+  },
+  {
+    "revision": "3aa41ba62fe63be99671",
+    "url": "/pseudotube/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pseudotube/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3aa41ba62fe63be99671",
+    "url": "/pseudotube/static/js/main.ceb6a6ba.chunk.js"
+  },
+  {
+    "revision": "382d2e3b60f8f304fed1",
+    "url": "/pseudotube/static/js/runtime~main.dcdc3ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pseudotube/static/media/logo.5d5d9eef.svg"
+  }
+]);
