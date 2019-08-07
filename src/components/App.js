3ebc2eefaeb3a2353e38ccdbfdd1b392085e44c9
shjoe <shure.joe33@gmail.com>
2019-08-06 18:51:49 -0700

    Added placeholder loader where VideoItem goes

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import SearchBar from './Searchbar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./Searchbar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = {
-        videos: [],
-        selectedVideo: null
-    }
-    handleSubmit = async (termFromSearchBar) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: termFromSearchBar,
-                part: 'snippet',
-                maxResults: 5,
-                key: 'AIzaSyCM6az2QF3X3hetkfs2vjOMUzZSD4v1h8g'
-            }
-        })
-        this.setState({
-            videos: response.data.items
-        })
-    };
-    handleVideoSelect = (video) => {
-        this.setState({ selectedVideo: video })
-    }
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+  handleSubmit = async termFromSearchBar => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: termFromSearchBar,
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyCM6az2QF3X3hetkfs2vjOMUzZSD4v1h8g"
+      }
+    });
+    this.setState({
+      videos: response.data.items
+    });
+  };
+  handleVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
 
-    render() {
-        return (
-            <div className='ui container' style={{ marginTop: '1em' }}>
-                <SearchBar handleFormSubmit={this.handleSubmit} />
-                <div className='ui grid'>
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos} />
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "1em" }}>
+        <SearchBar handleFormSubmit={this.handleSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        )
-    }
+            <div className="five wide column">
+              <VideoList
+                handleVideoSelect={this.handleVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React from "react";
 
 class Searchbar extends React.Component {
-    state = {
-        term: 'Search'
-    };
-    handleChange = (event) => {
-        this.setState({
-            term: event.target.value
-        });
-    };
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.handleFormSubmit(this.state.term);
-    }
+  state = {
+    term: "Search"
+  };
+  handleChange = event => {
+    this.setState({
+      term: event.target.value
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.handleFormSubmit(this.state.term);
+  };
 
-    render() {
-        return (
-            <div className='search-bar ui segment'>
-                <form onSubmit={this.handleSubmit} className='ui form'>
-                    <div className='field'>
-                        <label htmlFor="video-search">Search YouTube</label>
-                        <input onChange={this.handleChange} name='video-search' type="text" value={this.state.term}/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.handleSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="video-search">Search YouTube</label>
+            <input
+              onChange={this.handleChange}
+              name="video-search"
+              type="text"
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 export default Searchbar;
